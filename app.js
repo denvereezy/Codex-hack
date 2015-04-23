aeb9448ff376489e46eac8befc3273a6eecc756a
avermeulen <andre.vermeulen@gmail.com>
2015-04-23 12:34:50 +0200

    added public folder

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
 	res.render("index");
 });
 
- var server = app.listen(8080, function () {
+ var server = app.listen(4000, function () {
         var host = server.address().address;
         var port = server.address().port;
      	console.log('Example app listeng at http://%s:%s', host, port);
