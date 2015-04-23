3cc6c6e0f5cb1e85e7351aa92716b7b84589a954
denver <denver@projectcodex.co>
2015-04-23 12:28:09 +0200

    app

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+
+    // create a route
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+	res.render("index");
+});
+
+ var server = app.listen(8080, function () {
+        var host = server.address().address;
+        var port = server.address().port;
+     	console.log('Example app listeng at http://%s:%s', host, port);
+
+   });
