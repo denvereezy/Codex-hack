07b1b5cb7863ea61c4b3a6503b2b48ed54660824
avermeulen <andre.vermeulen@gmail.com>
2015-04-24 10:51:58 +0200

    checkboes for selection

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var bodyParser  = require('body-parser');
 var app = express();
 
 // create a route
+app.use(express.static('public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 var playlist = [];
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
-	res.render("index");
+	//res.render("index");
+});
+
+app.post('/test', function(req, res){
+	console.log("----> " + JSON.stringify(req.body));
 });
 
 app.get('/content', function(req, res){
@@ -40,7 +49,7 @@ app.get('/users', function(req, res){
 	res.render("users");
 });
 
- var server = app.listen(4000, function () {
+var server = app.listen(4000, function () {
         var host = server.address().address;
         var port = server.address().port;
      	console.log('Example app listeng at http://%s:%s', host, port);
